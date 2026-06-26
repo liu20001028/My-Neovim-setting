e58eb73acf916224c65d3c61ac175e2b3329df1d
想去海边 <149036874+liu20001028@users.noreply.github.com>
2026-06-26 23:17:59 +0800

    Delete nvim/lua/plugins directory

--- nvim/lua/plugins/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-  {"daltonmenezes/aura-theme"},
-{"navarasu/onedark.nvim"},
-  -- Configure LazyVim to load gruvbox
-
-}
--- nvim/lua/plugins/conda.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "kmontocam/nvim-conda",
-    dependencies = { "nvim-lua/plenary.nvim" }, -- 依赖自动安装
-    -- 可选：添加快捷键映射
-    keys = {
-        { "<leader>cn", ":CondaActivate<CR>", desc = "Conda 激活环境" }
-
-    }
-}
