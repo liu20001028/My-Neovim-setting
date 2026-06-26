e42518251e558fdf071d7e5991721fe59a0bbb0a
想去海边 <149036874+liu20001028@users.noreply.github.com>
2026-06-26 23:16:25 +0800

    Add files via upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- Lua
-require('onedark').setup {
-    style = 'warmer'
-}
-require('onedark').load()
+require('catppuccin').load()
